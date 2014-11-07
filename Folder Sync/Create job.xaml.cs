e7b8a5970905e67e61dda7a4649433163afea592
chrome289 <sidat289@gmail.com>
2014-11-07 23:01:52 +0530

    Added rest of the pages

---
+++ Folder Sync/Create job.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Ookii.Dialogs.Wpf;
+
+namespace Folder_Sync
+{
+    /// <summary>
+    /// Interaction logic for Create_job.xaml
+    /// </summary>
+    public partial class Create_job : Window
+    {
+        public Create_job()
+        {
+            InitializeComponent();
+            tb1.Text = "Empty";
+            tb2.Text = "Empty";
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog vd = new VistaFolderBrowserDialog();
+            vd.RootFolder = System.Environment.SpecialFolder.Desktop;
+            vd.ShowDialog();
+            tb1.Text = vd.SelectedPath;
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog vd = new VistaFolderBrowserDialog();
+            vd.RootFolder = System.Environment.SpecialFolder.Desktop;
+            vd.ShowDialog();
+            tb2.Text = vd.SelectedPath;
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Main m = new Main();
+            m.Show();
+        }
+    }
+}
